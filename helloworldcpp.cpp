8c736cd711485fd5949ac887d82c93d747a9b2b7
cortazar0kepfy <cantorzabellin@gmail.com>
2024-12-06 18:36:28 -0600

    pamsaka bikidit

---
+++ helloworldcpp.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+//función para saludar
+//es decir:
+void saludar(const std::string& nombre) 
+{
+	std::cout<<"Hola, "<<nombre<<"!"
+		std::endl;
+}
+
+
+
+int main()
+{
+	//Variables
+	int edad = 25;
+	std:: string nombre = "Juan";
+	saludar(nombre);
+
+
+	if(edad >= 18) {
+		std::cout <<"Eres mayor de edad."<< std::endl;
+	} else {
+		std::cout <<"Eres menor de edad"<< std::endl;
+	}
+//Bucle . . . . . . . . .  . . . . . . .
+//
+//
+for(int i = 0; i < 5; i++) {
+	std::cout <<"Iteración" << i <<
+		std::endl;
+}
+int notas[] = {10,20,30,40,50};
+for(int i = 0; i < 5; i++) {
+	std::cout <<"Nota" << i << ": "
+		<< notas[i] <<std::endl;
+}
+return 0;
+]
+
+

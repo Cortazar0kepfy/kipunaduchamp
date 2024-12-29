5b0554a8614d1c31ce8b6379879ead7c700eba3d
cortazar0kepfy <cantorzabellin@gmail.com>
2024-12-28 20:42:38 -0600

    salsa sin taco

--- _pythonEsfmbásico.py
+++ _pythonEsfmbásico.py
@@ -7,4 +7,32 @@
 """Esté es de nuevo un curso para poder poseer los conociminetos baásicos de python.
 """
 
+#funciones..
+"""Definiir y llamar funciones
+"""
+
+def saludar(nombre):
+    print(f"Estamos aqui, {nombre}! ")
+    
+saludar("Caguamo")
+
+#Listas y manipulación de listas
+"""Trabajar con listas y realizar operaciones comunes:
+"""
+frutas = ["Mamey", "Papaya", "Mango","Pitalla" "Rambután", "Kiwano"]
+print(frutas[0])
+
+
+frutas.append("Durazno") 
+""" 'append', va tener la función de añadir(manipulación), de ls lista.
+"""
+#Remove Elimina
+
+frutas.remove("Papaya")
+
+#Iterar sobre la lista..
+
+for fruta in frutas:
+    print(fruta)
+    
 

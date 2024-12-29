0b2850336321d6d760f50c999d573a7c0bd8ae33
cortazar0kepfy <cantorzabellin@gmail.com>
2024-12-28 18:40:33 -0600

    Flexo en tu dexo

---
+++ _pythonEsfmbásico.py
@@ -0,0 +1,10 @@
+#Cortázar Tinajero Luis Enrique.............
+
+
+# .    .      .          .          .           .             .             .        .
+
+
+"""Esté es de nuevo un curso para poder poseer los conociminetos baásicos de python.
+"""
+
+
---
+++ dale.py
@@ -0,0 +1,47 @@
+#Cortázar Tinajero Luis Enrique.
+
+
+print("En las almas del onizuka")
+
+
+#Variables y tipos de datos..
+
+"""Declarar variables de diferentes tipos y realizar operaciones básicas
+"""
+
+#Numeros.
+entero = 19
+
+flotante = 3.1416
+
+verdadero = True
+falso = False 
+print(entero, flotante,verdadero,falso)
+
+
+
+#Condicionales...
+
+"""Estructuras condicionales básicas
+"""
+edad = 18 
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+#Bucles
+"""Estructuras de bucles for y while
+"""
+
+for i in range(5):
+    print(f"holaaaaaa {i}")
+
+
+#Bucle while
+
+contador = 0
+while contador < 5:
+    print(f"Contador {contador}")
+    contador += 1   

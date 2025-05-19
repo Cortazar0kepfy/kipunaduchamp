290d4f293994cd853aee7091f4131d3879b8f489
cortazar0kepfy <cantorzabellin@gmail.com>
2025-05-19 11:39:17 -0600

    jajaja

--- Árbolde_lossueños.py
+++ Árbolde_lossueños.py
@@ -21,7 +21,12 @@ def arbol(i:float,angulo:float):
         tortuga.left(angulo) #Gira a la izquierda 80 grados.
         arbol(3.0*i/4.25,angulo)
         tortuga.right(2*angulo)
-        arbol(3.0*i/4.25,angulok)
+
+        arbol(3.0*i/4.25,angulo)
         tortuga.left(angulo)
         tortuga.backward(i)
 
+
+
+arbol(100,angulo)
+turtle.done()

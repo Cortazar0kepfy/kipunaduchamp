9707e91746032e5473f9fc51d96ddd1b6edc8631
cortazar0kepfy <cantorzabellin@gmail.com>
2025-06-11 10:50:33 -0600

    Hiels

--- zpsline.py
+++ zpsline.py
@@ -25,4 +25,20 @@ puntos = np.zeros(dim*nump,dtype=np.float64)
 
 
 # Parametrizaciónnnnnnnnnn
-X =np.lnspace(0.0,2.0*np.pi
+X =np.linspace(0.0,2.0*np.pi,nump+1)
+
+#Genera valores equidistantes.......
+
+"""Genera nump+1 puntos equidistantes en el intervalo[0,2pi[0, 2/pi].
+"""
+#Matematicamente significa que divide al intervalo.
+"""[0, 2pi][0. 2/pi]
+en partes iguales
+"""
+
+
+#Caracterizando la coordenada x:
+puntos[0:nump] = np.cos(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
+#Caracterizando a la coordenada y:
+puntos[nump:2*nump] = np.sin(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
+

ea0887eb485c5abd940e7d7285f04b34faa4cf95
cortazar0kepfy <cantorzabellin@gmail.com>
2025-06-11 09:58:02 -0600

    En el pargalope del ot}su

---
+++ zpsline.py
@@ -0,0 +1,28 @@
+#Cortázar Tinajero Luis Enrique.
+
+
+"""Se presenta un código cuyo objetivo es mostrar un el funcionamiento de la libreria numpy..
+"""
+
+
+import numpy as np
+from Curva import Curva
+"""Se presenta la libreria matplotlib.pyplot
+"""
+import math
+#Conjunto de puntossssss
+#....................
+nump:np.int32 = 8
+#Memoria para puntos...
+
+
+
+
+puntos = np.zeros(dim*nump,dtype=np.float64)
+
+
+
+
+
+# Parametrizaciónnnnnnnnnn
+X =np.lnspace(0.0,2.0*np.pi
